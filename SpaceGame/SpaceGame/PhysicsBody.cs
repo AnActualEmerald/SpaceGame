66bca65eb89d10617df2b9f39a6e246b5c29a67b
Burrito119 <kevinliger13@gmail.com>
2014-11-21 13:06:05 -0500

    Began implementing physics
    created the PhysicsBody wrapper class to act as a component for game
    objects

---
+++ SpaceGame/SpaceGame/PhysicsBody.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kgauthier16
+ * Date: 11/20/2014
+ * Time: 1:26 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using FarseerPhysics.Dynamics;
+
+namespace Game
+{
+	/// <summary>
+	/// Description of PhysicsBody.
+	/// </summary>
+	public class PhysicsBody : Body, Component
+	{
+		public PhysicsBody(World world, Vector2? position = null, float rotation = 0, object userdata = null, GameObject parent = null) 
+			: base(world, position, rotation, userdata)
+		{
+			this.parent = parent;
+		}
+		
+		public override void Input()
+		{
+			
+		}
+		
+		public override void Update()
+		{
+			
+		}
+		
+		public override void Render()
+		{
+			
+		}
+	}
+}
