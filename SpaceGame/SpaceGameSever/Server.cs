849ce50953e8b8e3fe911deeb46d0704955a50d6
Burrito119 <kevinliger13@gmail.com>
2014-11-25 12:10:00 -0500

    Began writing server
    Realized I wanted multiplayer, decided to start implementing that

---
+++ SpaceGame/SpaceGameSever/Server.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kgauthier16
+ * Date: 11/25/2014
+ * Time: 11:57 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace SpaceGameSever
+{
+	/// <summary>
+	/// Description of Server.
+	/// </summary>
+	public class Server
+	{
+		private List<Socket> clients = new List<Socket>();
+		
+		public Server()
+		{
+			
+		}
+	}
+}
