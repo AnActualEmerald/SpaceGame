67c51562e59e6726ef6c812246d2055614cf708b
Burrito119 <kevinliger13@gmail.com>
2015-03-24 09:41:43 -0400

    Started reworking the server
    Pretty good stuff, getting the hang of it I think

---
+++ SpaceGame/SpaceGameSever/Client.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Burrito119
+ * Date: 3/23/2015
+ * Time: 10:28 AM
+ * 
+ */
+using System;
+using System.Net.Sockets;
+using System.Net;
+using FarseerPhysics.Common;
+using Microsoft.Xna.Framework;
+
+namespace SpaceGameSever
+{
+	/// <summary>
+	/// Client object for containing info on clients that connect to the server
+	/// </summary>
+	public class Client
+	{
+		string name;
+		IPEndPoint remoteEP;
+		
+		/// <summary>
+		/// Creates Client object but does not initialize all the variables
+		/// </summary>
+		/// <param name="remoteEP">The IPEndPoint that the Client connected from</param>
+		/// <param name="name">The name that the Client sent when it connected. Defaults to "non"</param>
+		public Client(IPEndPoint remoteEP, string name = "non")
+		{
+			this.name = name;
+			this.remoteEP = remoteEP;
+		}
+
+		#region properties
+		
+		/// <summary>
+		/// The name of the client that connected to server
+		/// </summary>
+		public string Name {
+			get {
+				return name;
+			}
+			set {
+				name = value;
+			}
+		}
+
+		/// <summary>
+		/// The IPEndPoint that the client connected from
+		/// </summary>
+		public IPEndPoint RemoteEP {
+			get {
+				return remoteEP;
+			}
+			set {
+				remoteEP = value;
+			}
+		}
+		
+		#endregion
+		
+		#region methods
+		/// <summary>
+		/// Initializes client object and returns that object for use. Does modify original object
+		/// </summary>
+		/// <returns>Copy of initialized client object</returns>
+		public Client Init(byte[] texture_data, Vertices verts, Vector2 pos)
+		{
+			
+		}
+		
+		#endregion
+		
+	}
+}
--- SpaceGame/SpaceGameSever/Server.cs
+++ SpaceGame/SpaceGameSever/Server.cs
@@ -6,6 +6,11 @@
  * 
  */
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using FarseerPhysics.Common;
+using Microsoft.Xna.Framework;
 
 namespace SpaceGameSever
 {
@@ -14,8 +19,88 @@ namespace SpaceGameSever
 	/// </summary>
 	public class Server
 	{
-		public Server()
+		#region private members
+		private UdpListener _server;
+		private UdpListener _connecter;
+		private Thread connect_thread;
+		private bool running = false;
+		private Vector2 spawn_pos;
+		#endregion
+		
+		public List<Client> clients = new List<Client>();
+		
+		/// <summary>
+		/// Creates Server object and readies it for use
+		/// </summary>
+		/// <param name="toStart">Switch to decide if the Server should start once constructed</param>
+		public Server(bool toStart = false)
 		{
+			//TODO finish constructor implementation
+			_server = new UdpListener();
+			_connecter = new UdpListener();
+			connect_thread = new Thread(new ThreadStart(CheckForConn, 0));
+			if(toStart)
+				Start();
+		}
+		
+		public void Start()
+		{
+			running = true;
+			connect_thread.Start();
+			while(running)
+			{
+				//get info from clients(input, misc)
+				//step physics
+				//update clients
+			}
+		}
+		
+		private async void CheckForConn()
+		{
+			while(running){
+				Recieved r = await _connecter.ListenAsync();
+				Client c = new Client(r.Sender, r.message);
+				InitClient(ref c);
+				clients.Add(c);
+			}
+		}
+		
+		private async void InitClient(ref Client c)
+		{
+			byte[] tex;
+			Vertices verts;
+			await _connecter.SendAsync(c.RemoteEP, "sendtex");
+			Recieved r = await _connecter.ListenToAsync();
+			tex = Encoding.ASCII.GetBytes(r.message);
+			await _connecter.SendAsync(c.RemoteEP, "sendverts");
+			r = await _connecter.ListenToAsync();
+			verts = ParseClientVerts(r.message);
+			c.Init(tex, verts, spawn_pos);
+		}
+		
+		private Vertices ParseClientVerts(String vertstring)
+		{
+			Vertices v = new Vertices();
+			String[] points = vertstring.Split(';');
+			foreach(String p in points)
+			{
+				String[] xy = p.Split(',');
+				v.Add(new Vector2(float.Parse(xy[0]), float.Parse(xy[1])));
+			}
+			
+			return v;
+		}
+		
+		private void Input()
+		{
+			//TODO async?
+			//send request for input
+			//recieve and handle response
+		}
+		
+		private void Update()
+		{
+			
 		}
 	}
 }
